0cf9218ac6e255d90290f026dc1f16bd347c924e
Javier Segovia <jsegoviadev@gmail.com>
2021-06-09 16:55:33 -0400

    Initial commit (client base setup)

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">This is the start of client</h1>
+    </div>
+  )
+}
+
+export default IndexPage
