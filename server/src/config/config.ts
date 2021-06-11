9d076d4ecb361b8d6eb8e77cd8d2466424d35101
Javier Segovia <jsegoviadev@gmail.com>
2021-06-11 16:04:43 -0400

    DB config

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -3,15 +3,15 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const {
-  SERVER_HOSTNAME = 'localhost',
-  SERVER_PORT = 1337,
+  HOSTNAME = 'localhost',
+  PORT = 1337,
   DB_URL = 'mongodb://mongodb:27017',
 } = process.env
 
 console.log(DB_URL)
 
 export const config = {
-  hostname: SERVER_HOSTNAME,
-  port: Number(SERVER_PORT),
+  hostname: HOSTNAME,
+  port: Number(PORT),
   dbURL: DB_URL,
 }
